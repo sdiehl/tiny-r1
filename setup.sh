9c30d04344575d4541f9bb569826bee887a5157f
sdiehl <stephen.m.diehl@gmail.com>
2025-02-07 15:13:22 +0000

    setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
+# Install torch
+pip install "torch==2.5.1" --index-url https://download.pytorch.org/whl/cu121
+
+# Install flash-attn
+%pip install flash-attn 
+ 
 # Install base ML packages
-pip install unsloth vllm datasets transformers peft
+pip install unsloth datasets transformers peft
+
+# Install deepspeed
+pip install "deepspeed==0.15.4"
 
 # Install accelerate
-pip install accelerate
+pip install "accelerate==1.3.0"
+
+# Install vllm
+pip install "vllm==0.7.0"
 
 # Update Pillow
 pip install --upgrade pillow
