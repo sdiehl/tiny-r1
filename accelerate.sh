a4d1352eecaf196fb2d6eafc24b4fd798b02e15b
sdiehl <stephen.m.diehl@gmail.com>
2025-02-10 10:30:55 +0000

    deepspeed config

--- accelerate.sh
+++ accelerate.sh
@@ -4,5 +4,5 @@ set -e
 
 nohup accelerate launch \
   --num_processes 3 \
-  --config_file configs/accelerate_configs/deepspeed_zero3.yaml train_trl.py \
+  --config_file deepspeed_zero3.yaml train_trl.py \
   --config zero3.yaml
